3484f42086ca40d2b4754804a6f7ff66fa64963e
Tiago Santos <3266319+tiagolvsantos@users.noreply.github.com>
2023-12-27 18:53:23 +0000

    fix: gtrends lib

--- src/libs/google_trends_lib.py
+++ src/libs/google_trends_lib.py
@@ -4,7 +4,6 @@ pytrends = TrendReq(hl='en-US', tz=0)
 
 def get_keywords_trend(keywords_list: list):
     pytrends.build_payload(keywords_list, cat=0, timeframe='today 5-y')
-    pytrends.
     df_data = pytrends.interest_over_time() 
     df_data = df_data.reset_index()
     return df_data
